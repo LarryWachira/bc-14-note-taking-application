1df5cab6e580ce6d7ec9fbe2d4239d932434b22d
Lawrence Wachira <larrywachira@gmail.com>
2017-01-25 21:12:38 +0300

    Added number verification to check 'items_per_page' arguments

--- controller/functions.py
+++ controller/functions.py
@@ -5,12 +5,10 @@ def list_to_string(args):
     note_content = ' '.join(note_list)
     return note_content
 
-
 def query_to_string(args):
     query_list = args['<query_string>']
     query_string = ' '.join(query_list)
     return query_string
-
 
 def num_check(arg):
     num_str = arg['<note_id>']
@@ -18,5 +16,14 @@ def num_check(arg):
         note_id = int(num_str)
         return note_id
     except ValueError:
+        print('Please type in an integer value.')
+        return
+
+def num_check_limit(arg):
+    num_str = arg['<items_per_page>']
+    try:
+        items_per_page = int(num_str)
+        return items_per_page
+    except (ValueError, TypeError):
         print('Please type in an integer value.')
         return
