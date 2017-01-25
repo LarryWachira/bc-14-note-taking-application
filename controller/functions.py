f3c8798438481b40a1e649b90bc4c7a0fd3b39e6
Lawrence Wachira <larrywachira@gmail.com>
2017-01-25 13:34:58 +0300

    Added number check and string conversion functions -Adds a function to check that an argument is an integer and handle the error if it is not -Adds a function that coverts a list of arguments to a single string

--- controller/functions.py
+++ controller/functions.py
@@ -1,6 +1,22 @@
 
+
 def list_to_string(args):
     note_list = args['<note_content>']
     note_content = ' '.join(note_list)
     return note_content
 
+
+def query_to_string(args):
+    query_list = args['<query_string>']
+    query_string = ' '.join(query_list)
+    return query_string
+
+
+def num_check(arg):
+    num_str = arg['<note_id>']
+    try:
+        note_id = int(num_str)
+        return note_id
+    except ValueError:
+        print('Please type in an integer value.')
+        return
